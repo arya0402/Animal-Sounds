d159b47e7e22ac005392b76ce588da1e8fdde81f
Aryadolphin <78508957+arya0402@users.noreply.github.com>
2022-05-24 16:12:05 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,44 @@
 function startClassification() {
-    navigator.mediaDevices.getUserMedia({audio: true});
+    navigator.mediaDevices.getUserMedia({ audio: true });
     classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/spVuMkNHH/model.json', modelLoaded);
 }
 function modelLoaded() {
     classifier.classify(gotResults);
 }
 
+var lion = 0;
+var dolphin = 0;
+var horse = 0;
+var background noise = 0;
+
 function gotResults(error, results) {
-    console.log("got result");
+    if (error) {
+        console.error(error);
+    }
+    else {
+        console.log(results);
+        random_r = Math.floor(Math.random() * 255) + 1;
+        random_g = Math.floor(Math.random() * 255) + 1;
+        random_b = Math.floor(Math.random() * 255) + 1;
+        document.getElementById("name_animal_sound").innerHTML = "Name of the Animal Sound:" + results[0].label;
+        document.getElementById("sound").innerHTML = "Number of Times Animal Sound Detected: "(results[0].confidence * 100).toFixed(2) + " %";
+        document.getElementById("sound").style.color = "rgb(" + random_r + "," + random_g + "," + random_b + ")";
+    }
+
+    img = document.getElementById("img_display");
+
+    if (results[0].label == "Roaring") {
+        img.src = "lion.jpeg";
+    }
+    else if (results[0].label == "Whistles") {
+        img.src = "dolphin.jpeg";
+    }
+    else if (results[0].label == "Neighing") {
+        img.src = "horse.jpeg";
+    }
+    else {
+        img.src = "background noise.jpeg";
+    }
+
+
 }
