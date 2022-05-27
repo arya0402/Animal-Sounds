2d4c8b2dc8f12816c6aa0a21e4e89db59ae0d1fe
Aryadolphin <78508957+arya0402@users.noreply.github.com>
2022-05-27 18:04:32 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -21,23 +21,28 @@ function gotResults(error, results) {
         random_g = Math.floor(Math.random() * 255) + 1;
         random_b = Math.floor(Math.random() * 255) + 1;
         document.getElementById("name_animal_sound").innerHTML = "Name of the Animal Sound:" + results[0].label;
-        document.getElementById("sound").innerHTML = "Number of Times Animal Sound Detected: " + (results[0].confidence * 100).toFixed(2) + " %";
         document.getElementById("sound").style.color = "rgb(" + random_r + "," + random_g + "," + random_b + ")";
-    }
 
-    img = document.getElementById("img_display");
+        img = document.getElementById("img_display");
 
-    if (results[0].label == "Roaring") {
-        img.src = "lion.jpeg";
-    }
-    else if (results[0].label == "whistles") {
-        img.src = "dolphin.jpeg";
-    }
-    else if (results[0].label == "neighing") {
-        img.src = "horse.jpeg";
-    }
-    else {
-        img.src = "backgroundNoise.jpeg";
+        if (results[0].label == "Roaring") {
+            img.src = "lion.jpeg";
+            lion = lion + 1;
+            document.getElementById("sound").innerHTML = "Number of Times Animal Sound Detected: " + lion;
+        }
+        else if (results[0].label == "whistles") {
+            img.src = "dolphin.jpeg";
+            dolphin = dolphin + 1;
+            document.getElementById("sound").innerHTML = "Number of Times Animal Sound Detected: " + dolphin;
+        }
+        else if (results[0].label == "neighing") {
+            img.src = "horse.jpeg";
+            horse = horse + 1;
+            document.getElementById("sound").innerHTML = "Number of Times Animal Sound Detected: " + horse;
+        }
+        else {
+            img.src = "backgroundNoise.jpeg";
+        }
     }
 
 
